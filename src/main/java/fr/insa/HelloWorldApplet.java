ce7f9cdfca3c22649518de3ca9ec471c55de4790
Taha Khetib <khetibh@gmail.com>
2026-09-14 19:46:44 +0200

    Fix : added back the p in public

--- src/main/java/fr/insa/HelloWorldApplet.java
+++ src/main/java/fr/insa/HelloWorldApplet.java
@@ -2,7 +2,7 @@ package fr.insa;
 
 import javacard.framework.*;
 
-ublic class HelloWorldApplet extends Applet {
+public class HelloWorldApplet extends Applet {
 
     private static final byte[] HELLO_WORLD = {
         (byte)'H', (byte)'e', (byte)'l', (byte)'l', (byte)'o',
